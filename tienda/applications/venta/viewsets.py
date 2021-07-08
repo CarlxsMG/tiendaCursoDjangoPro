6efcfb1678d4eb41607036cb76a8485971f90c36
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-07-08 20:42:02 +0200

    Created create to ViewSet

--- tienda/applications/venta/viewsets.py
+++ tienda/applications/venta/viewsets.py
@@ -1,3 +1,6 @@
+# Python imports
+from django.utils.timezone import now
+
 # Django Rest Framework imports
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -14,22 +17,69 @@ from applications.producto.models import Product
 class VentasViewSet(viewsets.ViewSet):
     queryset = Sale.objects.all()
     
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = [IsAuthenticated]
 
     def list(self, request): # Requerido para el viewsets.ViewSet
         queryset = Sale.objects.all()
+        serializer = VentaReporteSerializers(queryset, many=True)
+
+        return Response(serializer.data)
+
+    def create(self, request, pk=None): # opcional para el viewsets.ViewSet
+        serializer = ProcesoVentaSerializer2(data=request.data)
+        serializer.is_valid(raise_exception=True)
         
-        return Response({'ok':'ok'})
+        # recuperar productos de una venta
+        products = Product.objects.filter(
+            id__in=serializer.validated_data['productos']
+        )
+
+        cantidades = serializer.validated_data['cantidades']
+        
+        venta = Sale.objects.create(
+            date_sale=now(),
+            amount=0,
+            count=0,
+            type_invoce=serializer.validated_data['type_invoce'],
+            type_payment=serializer.validated_data['type_payment'],
+            adreese_send=serializer.validated_data['adreese_send'],
+            user=self.request.user,
+        )
+
+        # Variables from ventas
+        amount, count = 0,0
+        
+        ventas_detalle = []
+
+        for producto, cantidad in zip(products, cantidades):
+
+            venta_detalle = SaleDetail(
+                sale=venta,
+                product=producto,
+                count=cantidad,
+                price_purchase=producto.price_purchase,
+                price_sale=producto.price_sale
+            )
+
+            ventas_detalle.append(venta_detalle)
+
+            amount = amount + (producto.price_sale * cantidad)
+            count = count + cantidad
+
+        venta.amount = amount
+        venta.count = count
+        venta.save()
+
+        SaleDetail.objects.bulk_create(ventas_detalle)
+
+        return Response(serializer.data)
 
     def retrieve(self, request, pk=None): # Requerido para el viewsets.ViewSet
         return Response({'ok':'ok2'})
 
     def update(self, request, pk=None): # opcional para el viewsets.ViewSet
         return Response({'ok':'ok3'})
-
-    def create(self, request, pk=None): # opcional para el viewsets.ViewSet
-        return Response({'ok':'ok4'})
 
     def destroy(self, request, pk=None): # opcional para el viewsets.ViewSet
         return Response({'ok':'ok5'})
