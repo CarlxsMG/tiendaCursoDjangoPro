61e736e7c4fd4681e989e970cd3e46fed61dceaf
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-07-08 19:52:24 +0200

    Created preform_create, .._update, .._destroy to ViewSet

--- tienda/applications/producto/viewsets.py
+++ tienda/applications/producto/viewsets.py
@@ -1,5 +1,6 @@
 # Django Rest Framework imports
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 # Local imports
 from .models import Colors, Product
@@ -23,3 +24,25 @@ class ProductViewSet(viewsets.ModelViewSet): # ESTO ES UN CRUD
     queryset = Product.objects.all()    # Requerido por el ViewSet
 
     pagination_class = PaginationSerializer
+
+
+    def perform_create(self, serializer):
+
+        if str(serializer.validated_data['video']) == "":
+            serializer.validated_data['video'] = "https://www.youtube.com/"
+
+        serializer.save(
+            video=serializer.validated_data['video']
+        )
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        instance.delete()
+
+    def list(self,request,*args, **kwargs):
+        queryset = Product.objects.productos_por_user(self.request.user)
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data)
